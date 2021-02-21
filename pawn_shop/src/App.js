58a741765f937a75a0c6939153270a2c401e0d6a
Gledinburgh <gledinburgh@gmail.com>
2021-02-21 12:27:14 -0600

    addapted App, index and styling to account for new advert feature

--- pawn_shop/src/App.js
+++ pawn_shop/src/App.js
@@ -1,21 +1,47 @@
 import './App.css';
-import {Layout} from 'antd';
-import ItemGrid from './components/ItemGrid'
-const { Header, Footer, Content} = Layout;
-const style = {background: "#ff517d", 'font-family': 'Suez One', color: '#ff4775', 'font-size': '250%'}
 
+import ReactDOM from 'react-dom';
+import {React, useState, useEffect} from 'react'
+
+
+import {Layout, notification, Image} from 'antd';
+import ItemGrid from './components/ItemGrid';
+import FooterAdvert from './components/FooterAdvert'
+import { SmileOutlined } from '@ant-design/icons';
+import showPromiseConfirm from './components/ModalAdvert'
+
+const { Header, Footer, Content} = Layout;
+const headstyle = {background: "#ff517d", 'font-family': 'Suez One', 'font-size': '200%', position: 'sticky'}
+const footstyle = {background: "#ffff62", 'font-family': 'Suez One', color: '#ff4775', 'font-size': '16px', 'text-align': 'center'}
 function App() {
+
+  const openNotification = () => {
+    notification.open({
+      duration: null,
+      onClick: showPromiseConfirm,
+      className:'ad',
+      closeIcon: <button className="advert right"> X </button>,
+      icon: <button className="advert left"> AD</button>,
+    });
+  };
+
+
+
   return (
-    <div className="wrapper">
-      <Header style={style} >
-        <logo ></logo>
+    <>
+    <FooterAdvert />
+    <div className="wrapper" onClick={openNotification}>
+
+      <Header style={headstyle} >
+
         <div>PAWN SHOP</div>
       </Header>
       <Content>
         <ItemGrid/>
       </Content>
-      <Footer>Footer</Footer>
     </div>
+      <Footer style={footstyle}> Careers / About Us / Returns </Footer>
+      </>
   );
 }
 
