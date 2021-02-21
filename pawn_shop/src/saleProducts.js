aa95d169d785fcf63e687c79eba158920df9ef84
Gledinburgh <gledinburgh@gmail.com>
2021-02-21 12:23:22 -0600

    created product list with image, and discriptive properties

---
+++ pawn_shop/src/saleProducts.js
@@ -0,0 +1,48 @@
+var products = [
+  {
+    file:'product_BMO.jpg',
+    name: 'BMO',
+    discription: 'Great Personality',
+    price: '300'
+},
+{
+  file:'product_LADY.jpeg',
+  name:'Mona Lisa',
+  discription:'Signed by Mona Lisa',
+  price: '200,000,000,000'
+},
+{
+  file:'product_NUK.jpg',
+  name:'Live Nuke',
+  discription:'cerca 1940\'s',
+  price: '300'
+},
+{
+  file:'product_STRO_087.jpg',
+  name:'Older Persons Stereo',
+  discription:'Has casset player',
+  price: '300'
+},
+{
+  file:'product_UGLY.jpg',
+  name:'Necronomicon',
+  discription:'Genuine Leather',
+  price: '300'
+},
+{
+  file:'product_XBOX_03.jpg',
+  name:'Cyborg head',
+  discription:'Teeth are in excelent condition',
+  price: '300'
+},
+{
+  file:'product_guitar.jpg',
+  name:'Gretch Resonator',
+  discription:'Previously owned by ...',
+  price: '300'
+},
+ ];
+
+
+export default products;
+
