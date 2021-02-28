cfd86d337a3dbe5a24a33ae9f9a0c6d2b97cdd30
Gledinburgh <gledinburgh@gmail.com>
2021-02-27 23:38:00 -0600

    refactored character list

---
+++ pawn_shop/src/data/characterInfo.js
@@ -0,0 +1,46 @@
+const characterInfo = [
+  {
+    Name:'Jesus',
+    Discription: 'Some words about the character',
+    Row: 'one',
+    File: 'Jesus.png',
+    Alt: 'Holy Relapse Title',
+    Id: 'Jesus'
+
+  },
+  {
+    Name:'Plomo',
+    Discription: 'Some words about the character',
+    Row: 'two',
+    File: 'Plomo.png',
+    Alt: 'Plomo character with evil smile"',
+    Id: 'Plomo'
+  },
+  {
+    Name:'Plata',
+    Discription: 'Some words about the character',
+    Row: 'one',
+    File: 'Plata.png',
+    Alt: 'Plata character with evil smile',
+    Id: "Plata"
+  },
+  {
+    Name:'Broker',
+    Discription: 'Some words about the character',
+    Row: 'two',
+    File: 'Broker.png',
+    Alt: 'Pawn shop broker character with reading newsPaper',
+    Id: 'Broker'
+  },
+  {
+    Name:'Crime Wave',
+    Discription: 'Some words about the character',
+    Row: 'one',
+    File: 'Crime_Wave.png',
+    Alt: 'Crime Wave character holding bag of drugs',
+    Id: 'crime-wave'
+  },
+
+]
+
+export default characterInfo;
