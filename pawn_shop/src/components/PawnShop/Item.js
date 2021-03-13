48b4ca5a2ca74ab478605d15dcf7d1adcd7ea259
Gledinburgh <gledinburgh@gmail.com>
2021-03-13 12:22:59 -0600

    item descriptions expand and contract behaviours now take place independently' '

---
+++ pawn_shop/src/components/PawnShop/Item.js
@@ -0,0 +1,44 @@
+import {Card} from 'antd';
+import {useState} from 'react';
+import ProductImage from './ProductImage';
+
+const Item = ({product}) => {
+
+  const [expandDescription, setExpandDescription] = useState(0)
+
+  const toggleDescriptionExpansion = (event, flag) => {
+    console.log(event.target.className)
+    if (flag === 0) {
+      setExpandDescription(1);
+    } else if (event.target.className === 'card-drop' || event.target.className === 'down') {
+
+      setExpandDescription(0);
+    }
+  }
+
+
+  return (
+
+    <div
+    key={product.Name}
+    className="item"
+    >
+      <Card
+      onClick={(event) => toggleDescriptionExpansion(event, expandDescription)}
+      ontransitionend ={() => toggleDescriptionExpansion(0)}
+      cover={<div className="container">
+                <ProductImage productInfo={product}/>
+                <div className="price">${product.Price}</div>
+             </div>
+        } >
+        <div className="card-title">{product.Name}</div>
+        <div expandDescription={expandDescription} className="product-description"> {product.Description}</div>
+        <button className="card-drop"><div expandDescription={expandDescription} className="down">▼</div></button>
+      </Card>
+</div>
+)
+
+}
+
+
+export default Item;
--- pawn_shop/src/components/PawnShop/ItemList.js
+++ pawn_shop/src/components/PawnShop/ItemList.js
@@ -1,39 +1,16 @@
 import { Row, Card} from 'antd';
 import {useState} from 'react';
 import ProductImage from './ProductImage';
+import Item from './Item'
 
 const ItemList = ({productInfo}) => {
-  const [expandDescription, setExpandDescription] = useState(0);
+
 
   return (
     <Row className="scroll" gutter={4}>
     {productInfo.map((product) => {
       return (
-        <div
-         key={product.Name}
-         className="item"
-         >
-      <Card
-        onClick={() => setExpandDescription(1)}
-        ontransitionend ={() => setExpandDescription(0)}
-        cover={
-        <div className="container">
-          <ProductImage productInfo={product}/>
-          <div className="price">${product.Price}</div>
-        </div>
-         } >
-          <div className="card-title">{product.Name}</div>
-          <div
-            expandDescription={expandDescription}
-            className="product-description"> {product.Description}</div>
-      <button className="card-drop">
-        <div
-          expandDescription={expandDescription}
-          className="down">▼</div>
-
-        </button>
-      </Card>
-    </div>
+      <Item product={product} />
 
     )
     })}
